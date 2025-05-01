b336eb3662f9e602f4b89e15d9df8963e1f368bf
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2025-05-01 14:31:16 +0800

    updated project to Unity 6 and setup relay

--- Assets/Scripts/Network/NetworkGameManager.cs
+++ Assets/Scripts/Network/NetworkGameManager.cs
@@ -1,31 +1,52 @@
 using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class NetworkGameManager : MonoBehaviour
 {
+    [SerializeField] bool _useRelay;
     [SerializeField] TextMeshProUGUI _networkSystemLabel;
-    private bool _isInitialized;
-    private void Update()
+
+    public void StartHost(int maxConnections = 2)
     {
-        if (_isInitialized) { return; }
-        if (Input.GetKeyDown(KeyCode.Alpha0))
+        StartHostWithRelay();
+    }
+    public async void StartHostWithRelay(int maxConnections = 2, string connectionType = "udp")
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
         {
-            NetworkManager.Singleton.StartServer();
-            _networkSystemLabel.text = $"Network System: Server";
-            _isInitialized = true;
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
+        var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        NetworkManager.Singleton.StartHost();
+        _networkSystemLabel.text = $"Network System: Host\nJoin Code: {joinCode}";
+    }
+
+    public void JoinCode(TMP_InputField joinCodeInputField)
+    {
+        if (string.IsNullOrEmpty(joinCodeInputField.text)) { return; }
+        StartClientWithRelay(joinCodeInputField.text);
+    }
+
+    private async void StartClientWithRelay(string joinCode, string connectionType = "udp")
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
         {
-            NetworkManager.Singleton.StartHost();
-            _networkSystemLabel.text = $"Network System: Host";
-            _isInitialized = true;
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            NetworkManager.Singleton.StartClient();
-            _networkSystemLabel.text = $"Network System: Client";
-            _isInitialized = true;
-        }
+
+        var allocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
+        NetworkManager.Singleton.StartClient();
+        _networkSystemLabel.text = $"Network System: Client\nJoin Code: {joinCode}";
     }
 }
